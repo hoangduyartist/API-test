00b61643ce5ecc97e89842b454a01327d5ba9439
hoangduyartist <hoangduy.artist@gmail.com>
2019-07-22 17:07:37 +0700

    fix angular-cors-request

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.set("views", "./views");
 app.use(bodyParser.json()); //using bodypaser as middleWave
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', webAPI);
+app.all("/api/*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    return next();
+});
 
 app.get('/', (req,res)=>res.render("index"))
 app.get('/test', (req,res)=>res.send({status: "status of response : 1 (expected); 0 (unexpected)", message:"message of response", data:"retrieve data if it exist"}))
